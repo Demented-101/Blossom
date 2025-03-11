4a6e0a6eb8887a68921c5c1684636029cd83ba3a
Galionare <galion1325@gmail.com>
2025-03-11 17:03:29 +0000

    MainMenu Fixed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,66 +7,13 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    public TextMeshProUGUI blossomText; 
-    public TextMeshProUGUI instructionsLabel;
-    public TextMeshProUGUI instructionsParagraph; 
-    public GameObject backButton; 
-    public GameObject settingsButton; 
-    public GameObject playButton; 
-    public GameObject rulesButton; 
-
     public void PlayGame()
     {
         SceneManager.LoadScene("Garden");  
     }
 
-    public void HideBlossomText()
+    public void QuitGame()
     {
-        blossomText.enabled = false;
-    }
-
-    public void ShowBlossomText()
-    {
-        blossomText.enabled = true;
-    }
-
-    public void SettingsPlayRulesDisable()
-    {
-        settingsButton.SetActive(false); 
-        playButton.SetActive(false); 
-        rulesButton.SetActive(false); 
-        backButton.SetActive(false); 
-
-    }
-
-    public void SettingsPlayRulesEnable()
-    {
-        settingsButton.SetActive(true); 
-        playButton.SetActive(true); 
-        rulesButton.SetActive(true); 
-        backButton.SetActive(false); 
-
-    }
-
-    public void EnableBackButton()
-    {
-        backButton.SetActive(true); 
-    }
-
-    public void HideBackButton()
-    {
-        backButton.SetActive(false); 
-    }
-
-    public void HideInstructions()
-    {
-        instructionsLabel.enabled = false;
-        instructionsParagraph.enabled = false; 
-    }
-
-    public void ShowInstructions()
-    {
-        instructionsLabel.enabled = true;
-        instructionsParagraph.enabled = true; 
+        Application.Quit();
     }
 }
