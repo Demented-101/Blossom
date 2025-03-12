dd4ccdf989d54669ee86c19ac76671026c38091a
Galionare <galion1325@gmail.com>
2025-03-12 00:37:13 +0000

    Implemented settings into player prefab

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -9,7 +10,22 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Garden");  
+        if(PlayerPrefs.GetInt("loadSaved") == 1)
+        {
+            SceneManager.LoadScene(PlayerPrefs.GetInt("LastScene"));
+        }
+        else
+        {
+            SceneManager.LoadScene("Garden");
+        }
+
+    }
+    public void MainScene()
+    {
+        Time.timeScale = 1f;
+        PlayerPrefs.SetInt("loadSaved", 1);
+        PlayerPrefs.SetInt("LastScene", SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene("UI");
     }
 
     public void QuitGame()
