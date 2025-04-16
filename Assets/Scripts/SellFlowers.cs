6eb21aa0fc4dfc49a5642e543e97da71b7ad872e
Hafiza <sonyapple18@gmail.com>
2025-04-16 11:48:47 +0100

    Shop - selling flowers, showing profit and movement done

---
+++ Assets/Scripts/SellFlowers.cs
@@ -0,0 +1,230 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SellFlowers : MonoBehaviour
+{
+    public Button toggleButton;
+    public Button sellButton; 
+
+    public Sprite menuSprite;  
+    public Sprite crossSprite; 
+    
+
+    public GameObject theBackground; 
+
+    [HideInInspector]
+    public GameObject[] lily, daisy, foxglove, orchid, sweetpea, tulip, rose, nightshade; 
+
+    private bool isMenuOpen = false;
+    private Image buttonImage;
+
+    
+
+    public TMP_Text noteText; 
+    public TMP_Text inStock; 
+    public TMP_Text numberSold; 
+    public TMP_Text profitText; 
+    public TMP_Text tulipStock, foxgloveStock, nightshadeStock, roseStock, daisyStock, lotvStock, orchidStock, sweetpeaStock; 
+    public TMP_Text tulipSold, foxgloveSold, nightshadeSold, roseSold, daisySold, lotvSold, orchidSold, sweetpeaSold; 
+
+
+    void Start()
+    {
+        buttonImage = toggleButton.GetComponent<Image>();
+        toggleButton.onClick.AddListener(ToggleMenu);
+        FlowerCounter(); 
+        UpdateUI();
+    }
+
+    void ToggleMenu()
+    {
+        isMenuOpen = !isMenuOpen;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        if (isMenuOpen)
+        {
+            //enable all pop up menu elements 
+            buttonImage.sprite = crossSprite;
+            inStock.enabled = true;
+            numberSold.enabled = true;
+            theBackground.SetActive(true); 
+
+            tulipStock.enabled = true; 
+            foxgloveStock.enabled = true; 
+            nightshadeStock.enabled = true; 
+            roseStock.enabled = true; 
+            daisyStock.enabled = true; 
+            lotvStock.enabled = true; 
+            orchidStock.enabled = true; 
+            sweetpeaStock.enabled = true; 
+
+            tulipSold.enabled = true; 
+            foxgloveSold.enabled = true; 
+            nightshadeSold.enabled = true; 
+            roseSold.enabled = true; 
+            daisySold.enabled = true; 
+            lotvSold.enabled = true; 
+            orchidSold.enabled = true; 
+            sweetpeaSold.enabled = true; 
+
+            noteText.enabled = true; 
+            sellButton.enabled = true; 
+            profitText.enabled = true; 
+        }
+        else
+        {
+            //disable all pop up menu elements 
+            buttonImage.sprite = menuSprite;
+            inStock.enabled = false;
+            numberSold.enabled = false;
+            theBackground.SetActive(false); 
+
+            tulipStock.enabled = false; 
+            foxgloveStock.enabled = false; 
+            nightshadeStock.enabled = false; 
+            roseStock.enabled = false; 
+            daisyStock.enabled = false; 
+            lotvStock.enabled = false; 
+            orchidStock.enabled = false; 
+            sweetpeaStock.enabled = false; 
+
+            tulipSold.enabled = false; 
+            foxgloveSold.enabled = false; 
+            nightshadeSold.enabled = false; 
+            roseSold.enabled = false; 
+            daisySold.enabled = false; 
+            lotvSold.enabled = false; 
+            orchidSold.enabled = false; 
+            sweetpeaSold.enabled = false; 
+
+            noteText.enabled = false; 
+            sellButton.enabled = false; 
+            profitText.enabled = false; 
+        }
+    }
+
+    public void FlowerCounter()
+    {
+        //find all the flowers depending on their tags 
+        lily = GameObject.FindGameObjectsWithTag("LilyOfTheValley");
+        daisy = GameObject.FindGameObjectsWithTag("Daisy");
+        foxglove = GameObject.FindGameObjectsWithTag("Foxglove");
+        orchid = GameObject.FindGameObjectsWithTag("Orchid");
+        sweetpea = GameObject.FindGameObjectsWithTag("Sweetpea");
+        tulip = GameObject.FindGameObjectsWithTag("Tulip");
+        rose = GameObject.FindGameObjectsWithTag("Rose");
+        nightshade = GameObject.FindGameObjectsWithTag("Nightshade");
+
+
+        int lilyCount = lily.Length;
+        int daisyCount = daisy.Length;
+        int foxgloveCount = foxglove.Length;
+        int orchidCount = orchid.Length;
+        int sweetpeaCount = sweetpea.Length;
+        int tulipCount = tulip.Length;
+        int roseCount = rose.Length;
+        int nightshadeCount = nightshade.Length;
+
+        lotvStock.text = "Lily Of The Valley's: " + lilyCount;
+        daisyStock.text = "Daisies: " + daisyCount;
+        foxgloveStock.text = "Foxgloves: " + foxgloveCount;
+        orchidStock.text = "Orchids: " + orchidCount;
+        sweetpeaStock.text = "Sweetpeas: " + sweetpeaCount;
+        tulipStock.text = "Tulips: " + tulipCount;
+        roseStock.text = "Roses: " + roseCount;
+        nightshadeStock.text = "Nightshades: " + nightshadeCount;
+    }
+
+    public void SellButton()
+    {
+        int lilyRandomNumber = Random.Range(0, lily.Length); 
+        int daisyRandomNumber = Random.Range(0, daisy.Length); 
+        int foxgloveRandomNumber = Random.Range(0, foxglove.Length); 
+        int orchidRandomNumber = Random.Range(0, orchid.Length); 
+        int sweetpeaRandomNumber = Random.Range(0, sweetpea.Length); 
+        int tulipRandomNumber = Random.Range(0, tulip.Length); 
+        int roseRandomNumber = Random.Range(0, rose.Length); 
+        int nightshadeRandomNumber = Random.Range(0, nightshade.Length); 
+
+        lotvStock.text = "Lily Of The Valley's: " + (lily.Length - lilyRandomNumber);
+        daisyStock.text = "Daisies: " + (daisy.Length - daisyRandomNumber);
+        foxgloveStock.text = "Foxgloves: " + (foxglove.Length - foxgloveRandomNumber);
+        orchidStock.text = "Orchids: " + (orchid.Length - orchidRandomNumber);
+        sweetpeaStock.text = "Sweetpeas: " + (sweetpea.Length - sweetpeaRandomNumber);
+        tulipStock.text = "Tulips: " + (tulip.Length - tulipRandomNumber);
+        roseStock.text = "Roses: " + (rose.Length - roseRandomNumber);
+        nightshadeStock.text = "Nightshades: " + (nightshade.Length - nightshadeRandomNumber);
+
+        lotvSold.text = "Lily Of The Valley's: " + lilyRandomNumber;
+        daisySold.text = "Daisies: " + daisyRandomNumber;
+        foxgloveSold.text = "Foxgloves: " + foxgloveRandomNumber;
+        orchidSold.text = "Orchids: " + orchidRandomNumber;
+        sweetpeaSold.text = "Sweetpeas: " + sweetpeaRandomNumber;
+        tulipSold.text = "Tulips: " + tulipRandomNumber;
+        roseSold.text = "Roses: " + roseRandomNumber;
+        nightshadeSold.text = "Nightshades: " + nightshadeRandomNumber;
+
+        sellButton.enabled = false; 
+        noteText.enabled = false; 
+        int flowerTotal = (lilyRandomNumber + daisyRandomNumber + foxgloveRandomNumber + orchidRandomNumber + sweetpeaRandomNumber
+                        + tulipRandomNumber + roseRandomNumber + nightshadeRandomNumber); 
+
+        
+        //remove number of lotvs from scene 
+        for(int i =0;i<lilyRandomNumber;i++)
+        {
+            lily[i].SetActive(false); 
+        }
+
+        //remove number of daisies from scene 
+        for(int i =0;i<daisyRandomNumber;i++)
+        {
+            daisy[i].SetActive(false); 
+        }
+
+        //remove number of foxgloves from scene 
+        for(int i =0;i<foxgloveRandomNumber;i++)
+        {
+            foxglove[i].SetActive(false); 
+        }
+
+        //remove number of orchids from scene 
+        for(int i =0;i<orchidRandomNumber;i++)
+        {
+            orchid[i].SetActive(false); 
+        }
+
+        //remove number of sweetpeas from scene 
+        for(int i =0;i<sweetpeaRandomNumber;i++)
+        {
+            sweetpea[i].SetActive(false); 
+        }
+
+        //remove number of tulips from scene 
+        for(int i =0;i<tulipRandomNumber;i++)
+        {
+            tulip[i].SetActive(false); 
+        }
+
+        //remove number of roses from scene 
+        for(int i =0;i<roseRandomNumber;i++)
+        {
+            rose[i].SetActive(false); 
+        }
+
+        //remove number of nightshades from scene 
+        for(int i =0;i<nightshadeRandomNumber;i++)
+        {
+            nightshade[i].SetActive(false); 
+        }
+
+        profitText.text = "PROFIT: " + flowerTotal.ToString() + " X " + " £3 " + " = " + "£" + (flowerTotal * 3).ToString(); 
+
+    }
+}
