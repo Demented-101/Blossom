a2c04257d4a75d202f4a785dce7af8af33faa766
Demented <115271904+Demented-101@users.noreply.github.com>
2025-05-11 18:26:08 +0100

    Added Event senders to fix UI error

--- Assets/Scripts/AudioHandler.cs
+++ Assets/Scripts/AudioHandler.cs
@@ -7,6 +7,12 @@ public class AudioHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (GameObject.FindGameObjectsWithTag("musicPlayer").Length > 1)
+        {
+            GameObject.Destroy(gameObject);
+            return;
+        }
+
         DontDestroyOnLoad(this);
         gameObject.GetComponent<AudioSource>().Play();
     }
